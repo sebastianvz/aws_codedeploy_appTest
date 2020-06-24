07f9a813e1115b59a6b66bcb6fe4edee5a167e0e
Sebastián Velez <devsebastianvz@gmail.com>
2020-06-24 11:59:09 -0500

    aplicationStar change user

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-##### Start Service app
+##### Start Service pi
 
 cd /var/www/html/test_app_deploy/
 #####      Creating a service call doorSensor
-sudo forever-service install test_app_deploy -r app
+sudo forever-service install test_app_deploy -r pi
 ##### start a service called doorSensor
 sudo service test_app_deploy start
