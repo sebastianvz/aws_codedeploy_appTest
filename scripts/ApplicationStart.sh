60c8c9b8fc42ccaaf79c482025e311c17571b791
Sebastián Velez <devsebastianvz@gmail.com>
2020-06-24 12:10:25 -0500

    aplicationStar got to folder where is app.js

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -3,6 +3,6 @@
 
 cd /var/www/html/test_app_deploy/test_app
 #####      Creating a service call doorSensor
-#sudo forever-service install test_app_deploy -r pi
+sudo forever-service install test_app_deploy -r pi
 ##### start a service called doorSensor
-#sudo service test_app_deploy start
+sudo service test_app_deploy start
