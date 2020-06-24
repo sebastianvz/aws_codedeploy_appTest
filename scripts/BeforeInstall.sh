037ab136f7a94c081c6ac1edac5f612517f3ec01
Sebastián Velez <devsebastianvz@gmail.com>
2020-06-24 11:50:54 -0500

    beforeInstall

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -2,7 +2,7 @@
 
 #update the system
 
-rm -rf /var/www/html/test_app_deploy
+sudo rm -rf /var/www/html/test_app_deploy
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
+#sudo apt-get -y update
+#sudo apt-get -y upgrade
